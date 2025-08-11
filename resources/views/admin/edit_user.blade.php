06d67ad20d660f67d966f24ab27884e5c59fb468
Abdallahweb <121055533+Abdallahweb@users.noreply.github.com>
2025-08-11 06:57:41 +0300

    Update edit_user.blade.php

--- resources/views/admin/edit_user.blade.php
+++ resources/views/admin/edit_user.blade.php
@@ -6,42 +6,44 @@
 </head>
 
 <body>
-    <div id="app">
+<div id="app">
 @include('admin.header')
 
 <div class="main-content">
-        <section class="section">
-          <h1 class="section-header">
-            <div>Edit User</div>
+<section class="section">
+<h1 class="section-header">
+<div>Edit User</div>
 
 
 
-          </h1>
-          <div class="section-body">
-            <div class="row">
-              <div class="col-12 col-md-6 col-lg-12">
-                <div class="card">
+</h1>
+<div class="section-body">
+<div class="row">
+<div class="col-12 col-md-6 col-lg-12">
+<div class="card">
 
 
-                    </div>
-                    <div class="col-12 col-md-6 col-lg-12">
+</div>
+<div class="col-12 col-md-6 col-lg-12">
 
 
-                      @if ($errors->any())
-                      <div class="alert alert-danger">
-                          <ul>
-                              @foreach ($errors->all() as $error)
-                                  <li>{{ $error }}</li>
-                              @endforeach
-                          </ul>
-                      </div>
-                      @endif
+@if ($errors->any())
+<div>
+<ul>
+@foreach ($errors->all() as $error)
 
-                      <form class="" method="POST" action="{{url('update_user',$edit_user->id)}}">
-                @csrf
+<?php toastr()->closeButton()->timeOut(10000)->addError($error);
+?>
+@endforeach
+</ul>
+</div>
+@endif
 
-                  <div class="form-group">
-                    <label for="">Edit User</label>
+<form class="" method="POST" action="{{url('update_user',$edit_user->id)}}">
+@csrf
+
+<div class="form-group">
+<label for="">Edit User</label>
 
 
 <input class="form-control" type="text" name="name" value="{{$edit_user->name}}" placeholder="Name"><br>
@@ -50,27 +52,27 @@
 
 </div>
 
-                  <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-block" tabindex="4">
-                    Update
-                    </button>
-                  </div>
-                </form>
-              </div>
-
-
-                </div>
-
-                  </div>
+<div class="form-group">
+<button type="submit" class="btn btn-primary btn-block" tabindex="4">
+Update
+</button>
 </div>
-              </div>
+</form>
+</div>
 
-          </div>
-        </section>
-      </div>
-     </div>
 
-            @include('admin.footer')
+</div>
+
+</div>
+</div>
+</div>
+
+</div>
+</section>
+</div>
+</div>
+
+@include('admin.footer')
 
 
 </body>
